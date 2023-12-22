5c68ebdcc8f90b999cfc290a735e425104a98edd
Elina Kerola <elinak90@icloud.com>
2023-12-22 10:40:41 +0100

    code as a egenskap in team

--- backend/core/Models/Team.cs
+++ backend/core/Models/Team.cs
@@ -4,9 +4,11 @@ public class Team
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Code{get;set;}
     public DateTime CreatedAt { get; set; }
     public string TeamRole { get; set; }
     public List<User> Users { get; set; }
 
     public Team() { }
+
 }
