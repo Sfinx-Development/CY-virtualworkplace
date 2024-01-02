34a3d28fa559566b89881719d35c11fb39962018
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-01-02 21:24:50 +0100

    auth works with signing and sending jwt

---
+++ backend/api/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading.Tasks;
+using api;
+using core;
+using services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly JwtService _jwtService;
+
+        public UserController(JwtService jwtService)
+        {
+            _jwtService = jwtService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<User>> GetUserDTO()
+        {
+            try
+            {
+                Console.WriteLine("INNE I USER CONTROLLERN");
+                var jwt = HttpContext
+                    .Request.Headers["Authorization"]
+                    .ToString()
+                    .Replace("Bearer ", string.Empty);
+
+                Console.WriteLine(jwt);
+                if (string.IsNullOrWhiteSpace(jwt))
+                {
+                    return BadRequest("JWT token is missing.");
+                }
+                var loggedInUser = _jwtService.GetByJWT(jwt);
+
+                if (loggedInUser == null)
+                {
+                    return BadRequest("Failed to get user.");
+                }
+                return Ok(loggedInUser);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+    }
+}
---
+++ backend/services/LoginService.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using core;
+using Microsoft.IdentityModel.Tokens;
+
+namespace services
+{
+    public class LogInService
+    {
+        // private readonly ILogInDB<User> _logInUser;
+        private readonly SymmetricSecurityKey _securityKey;
+
+        public LogInService(SymmetricSecurityKey securityKey)
+        {
+            // _logInUser = logInUser;
+            _securityKey = securityKey;
+        }
+
+        public LogInDTO LogIn(string email, string passWord)
+        {
+            try
+            {
+                // User user = _logInUser.GetMemberByLogIn(email, passWord);
+                if (email == "kalle@mail.com" && passWord == "Hej123")
+                {
+                    Console.WriteLine("INNE I IF I LOGISERVICE");
+                    User user = new();
+                    user.Id = "12312481";
+                    user.Email = email;
+                    string jwtToken = GenerateJwtToken(user);
+
+                    LogInDTO logInDTO = new LogInDTO
+                    {
+                        Email = email,
+                        Password = passWord,
+                        JWT = jwtToken
+                    };
+
+                    return logInDTO;
+                }
+
+                return null;
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            try
+            {
+                Console.WriteLine("USERN: " + user.Email + user.Id);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Issuer = "CY-VirtualWorkplace",
+                    Audience = "api",
+                    Subject = new ClaimsIdentity(
+                        new Claim[]
+                        {
+                            new Claim(ClaimTypes.Name, user.Email),
+                            new Claim("UserId", user.Id.ToString())
+                        }
+                    ),
+                    Expires = DateTime.UtcNow.AddDays(14),
+                    SigningCredentials = new SigningCredentials(
+                        _securityKey,
+                        SecurityAlgorithms.HmacSha256Signature
+                    )
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return tokenHandler.WriteToken(token);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"error converting Base64 string: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
