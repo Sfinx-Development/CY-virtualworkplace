b8e7fcb272fe4bf864123fe6ec2f7276950911ed
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-01-04 10:49:47 +0100

    teamrepository

---
+++ backend/core/Data/Repositories/TeamRepository.cs
@@ -0,0 +1,96 @@
+using Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace core;
+
+public class TeamRepository : ITeamRepository
+{
+    private readonly CyDbContext _cyDbContext;
+
+    public TeamRepository(CyDbContext cyDbContext)
+    {
+        _cyDbContext = cyDbContext;
+    }
+
+    public async Task<Team> CreateAsync(Team team)
+    {
+        try
+        {
+            await _cyDbContext.Teams.AddAsync(team);
+            await _cyDbContext.SaveChangesAsync();
+
+            return team;
+        }
+        catch (Exception e)
+        {
+            throw new Exception();
+        }
+    }
+
+    public async Task<Team> GetByCodeAsync(string code)
+    {
+        try
+        {
+            Team team = await _cyDbContext.Teams.FirstAsync(t => t.Code == code);
+            if (team != null)
+            {
+                return team;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+
+    public async Task<Team> UpdateAsync(Team team)
+    {
+        try
+        {
+            var teamToUpdate = await _cyDbContext.Teams.FirstAsync(t => t.Id == team.Id);
+
+            if (teamToUpdate == null)
+            {
+                return null;
+            }
+
+            teamToUpdate.Name = team.Name ?? teamToUpdate.Name;
+            teamToUpdate.Code = team.Code ?? teamToUpdate.Code;
+            teamToUpdate.TeamRole = team.TeamRole ?? teamToUpdate.TeamRole;
+
+            _cyDbContext.Teams.Update(teamToUpdate);
+
+            await _cyDbContext.SaveChangesAsync();
+            return teamToUpdate;
+        }
+        catch (Exception e)
+        {
+            throw new Exception();
+        }
+    }
+
+    public async Task DeleteByIdAsync(string id)
+    {
+        try
+        {
+            //OBS!!!!!!!!!!
+            //sen behöver vi ta bort alla profiler som hör till teamet
+            //endast ägaren kan ta bort teamet - fixas i service klaasssen
+            var teamToDelete = await _cyDbContext.Teams.FindAsync(id);
+            var deletedTeam = teamToDelete;
+            if (teamToDelete != null)
+            {
+                _cyDbContext.Teams.Remove(teamToDelete);
+                await _cyDbContext.SaveChangesAsync();
+            }
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+}
