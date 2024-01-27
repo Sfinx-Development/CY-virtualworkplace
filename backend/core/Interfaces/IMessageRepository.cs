dd767989f4e4ed4048c3864273c8e28efabd24be
Elina Kerola <elinak90@icloud.com>
2024-01-27 11:11:43 +0100

    la till i interface

--- backend/core/Interfaces/IMessageRepository.cs
+++ backend/core/Interfaces/IMessageRepository.cs
@@ -5,6 +5,7 @@ namespace Interfaces;
 public interface IMessageRepository
 {
     Task<Message> CreateAsync(Message message);
+    Task<List<Message>> GetAllMessagesInConversation(string conversationParticipantId);
     // Task<Message> GetByIdAsync(string messageId);
     // Task<List<Message>> GetAllOccasionsByMeetingId(string id);
     // Task DeleteByIdAsync(string id);
