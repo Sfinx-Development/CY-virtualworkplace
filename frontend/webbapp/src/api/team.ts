f7f3d2e61d5ee61974a8fc29b0b416dbae7b7410
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-04-13 17:28:02 +0200

    request fixes för user och team

--- frontend/webbapp/src/api/team.ts
+++ frontend/webbapp/src/api/team.ts
@@ -26,61 +26,11 @@ export const FetchGetMyTeams = async (): Promise<Team[]> => {
   }
 };
 
-export const FetchGetMyTeamRequests = async (): Promise<TeamRequest[]> => {
-  try {
-    const response = await fetch(apiUrl + "/teamrequests", {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const responseBody = await response.json();
-
-    if (!response.ok) {
-      throw new Error("Något gick fel vid hämtning av teamrequests");
-    }
-
-    const data = responseBody.$values as TeamRequest[];
-    return data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
-
-export const FetchGetRequestsByTeamId = async (
-  teamId: string
-): Promise<TeamRequest[]> => {
-  try {
-    const response = await fetch(apiUrl + "/teamrequestsbyteam", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-
-      body: JSON.stringify(teamId),
-    });
-    const responseBody = await response.json();
-
-    if (!response.ok) {
-      throw new Error("Något gick fel vid hämtning av teamrequests");
-    }
-
-    const data = responseBody.$values as TeamRequest[];
-    return data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
-
 export const FetchCreateTeam = async (
   newTeam: CreateTeamDTO
 ): Promise<Team> => {
   try {
-    const response = await fetch(apiUrl + "/create", {
+    const response = await fetch(apiUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -140,9 +90,8 @@ export const FetchJoinTeam = async ({
 
 export const FetchUpdateTeam = async (team: Team): Promise<Team> => {
   try {
-    console.log("TEAMET: ", team);
-    const response = await fetch(apiUrl + "/update", {
-      method: "POST",
+    const response = await fetch(apiUrl, {
+      method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
@@ -157,54 +106,6 @@ export const FetchUpdateTeam = async (team: Team): Promise<Team> => {
     const data = await response.json();
 
     return data as Team;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
-
-export const FetchUpdateTeamRequest = async (
-  teamRequest: TeamRequest
-): Promise<TeamRequest> => {
-  try {
-    const response = await fetch(apiUrl + "/teamrequestupdate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      body: JSON.stringify(teamRequest),
-    });
-
-    if (!response.ok) {
-      throw new Error("Något gick fel vid att uppdatera teamrequest.");
-    }
-
-    const data = await response.json();
-
-    return data as TeamRequest;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
-
-export const FetchDeleteRequest = async (requestId: string) => {
-  try {
-    const response = await fetch(apiUrl + "/deleteteamrequest", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      body: JSON.stringify(requestId),
-    });
-
-    if (!response.ok) {
-      throw new Error("Något gick fel vid att readera förfrågan.");
-    }
-
-    return true;
   } catch (error) {
     console.error(error);
     throw error;
---
+++ frontend/webbapp/src/api/teamrequest.ts
@@ -0,0 +1,127 @@
+import { CreateTeamDTO, Team, TeamRequest } from "../../types";
+import { getApiUrl } from "./config";
+
+const apiUrl = getApiUrl() + "/teamrequest";
+
+export const FetchGetMyTeamRequests = async (): Promise<TeamRequest[]> => {
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const responseBody = await response.json();
+
+    if (!response.ok) {
+      throw new Error("Något gick fel vid hämtning av teamrequests");
+    }
+
+    const data = responseBody.$values as TeamRequest[];
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const FetchGetRequestsByTeamId = async (
+  teamId: string
+): Promise<TeamRequest[]> => {
+  try {
+    const response = await fetch(apiUrl + "/teamid", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(teamId),
+    });
+    const responseBody = await response.json();
+
+    if (!response.ok) {
+      throw new Error("Något gick fel vid hämtning av teamrequests");
+    }
+
+    const data = responseBody.$values as TeamRequest[];
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const FetchCreateTeam = async (
+  newTeam: CreateTeamDTO
+): Promise<Team> => {
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(newTeam),
+    });
+
+    if (!response.ok) {
+      throw new Error("Något gick fel vid skapandet av team.");
+    }
+
+    const data = await response.json();
+
+    return data as Team;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const FetchUpdateTeamRequest = async (
+  teamRequest: TeamRequest
+): Promise<TeamRequest> => {
+  try {
+    const response = await fetch(apiUrl + "/teamrequestupdate", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(teamRequest),
+    });
+
+    if (!response.ok) {
+      throw new Error("Något gick fel vid att uppdatera teamrequest.");
+    }
+
+    const data = await response.json();
+
+    return data as TeamRequest;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const FetchDeleteRequest = async (requestId: string) => {
+  try {
+    const response = await fetch(apiUrl + "/id", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(requestId),
+    });
+
+    if (!response.ok) {
+      throw new Error("Något gick fel vid att readera förfrågan.");
+    }
+
+    return true;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
