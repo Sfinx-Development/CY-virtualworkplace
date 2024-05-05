80afc034ed87d36c846bbf6d59a39f74b99f6cf4
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-05-05 22:23:10 +0200

    ny tom sida i navigation samt ikon

---
+++ frontend/webbapp/src/pages/StartSignedIn/Guide.tsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from "@mui/material";
+
+export default function Guide() {
+  return (
+    <Container sx={{ padding: "20px" }}>
+      <div style={{ textAlign: "center" }}>
+        <Typography variant="h4" gutterBottom>
+          GUIDE / INSTRUKTIONER
+        </Typography>
+      </div>
+    </Container>
+  );
+}
